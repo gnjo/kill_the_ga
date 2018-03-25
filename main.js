cf6abbc7324d16b9d86e1cdb0fa167fe3519ef1a
gunjyo <35440828+gnjo@users.noreply.github.com>
2018-03-25 12:14:16 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
  document.addEventListener("DOMContentLoaded",function(ev){main()})
  ;
  function main(){
-  console.log('boot extention');
+  console.log('boot extension');
   let killer=()=>{root.ga=(root.ga)?console.log('kill the ga'):null}
   setInterval(killer,1000);
  }
